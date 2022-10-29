1250fc67878d21b984f80b3fffeb3eaa2ecd69ad
jdeseva <zjmqydes@163.com>
2022-10-29 15:02:24 +0800

    init

---
+++ data/DoNotStarveTogether/Cluster_1/mods/dedicated_server_mods_setup.lua
@@ -0,0 +1,41 @@
+--There are two functions that will install mods, ServerModSetup and ServerModCollectionSetup. Put the calls to the functions in this file and they will be executed on boot.
+
+--ServerModSetup takes a string of a specific mod's Workshop id. It will download and install the mod to your mod directory on boot.
+	--The Workshop id can be found at the end of the url to the mod's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
+	--ServerModSetup("350811795")
+
+--ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
+	--The Workshop id can be found at the end of the url to the collection's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
+	--ServerModCollectionSetup("379114180")
+
+	ServerModSetup("1185229307")
+	ServerModSetup("1207269058")
+	ServerModSetup("1392778117")
+	ServerModSetup("1530801499")
+	ServerModSetup("1571581787")
+	ServerModSetup("1595631294")
+	ServerModSetup("1909182187")
+	ServerModSetup("1951468597")
+	ServerModSetup("1991746508")
+	ServerModSetup("2066838067")
+	ServerModSetup("2078243581")
+	ServerModSetup("2121078176")
+	ServerModSetup("2174620235")
+	ServerModSetup("2208128427")
+	ServerModSetup("2287303119")
+	ServerModSetup("2323630362")
+	ServerModSetup("2347908689")
+	ServerModSetup("2427481232")
+	ServerModSetup("2505341606")
+	ServerModSetup("2510473186")
+	ServerModSetup("2582670245")
+	ServerModSetup("2621753956")
+	ServerModSetup("2713622593")
+	ServerModSetup("2798599672")
+	ServerModSetup("375859599")
+	ServerModSetup("378160973")
+	ServerModSetup("458587300")
+	ServerModSetup("631648169")
+	ServerModSetup("875994715")
